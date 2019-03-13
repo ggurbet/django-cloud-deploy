ed44e2b9b0c1bcfabc66d15edbb107de812425df
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2019-03-13 15:02:08 -0700

    Add a parser for requirements.txt.

--- django_cloud_deploy/skeleton/requirements_parser.py
+++ django_cloud_deploy/skeleton/requirements_parser.py
@@ -11,3 +11,41 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""A module help parse requirements.txt."""
+import os
+import re
+from typing import Set
+
+
+def parse(path: str) -> Set[str]:
+    """Parses requirements given the absolute path of a requirements.txt.
+
+    Note that this function only returns what are the requirements. It will not
+    return the version restrictions.
+
+    Args:
+        path: Absolute path of a requirements.txt.
+
+    Returns:
+        A list of requirements from the requirements.txt.
+    """
+
+    results = set()
+    if not os.path.exists(path):
+        return results
+
+    dir_path = os.path.dirname(path)
+    with open(path) as requirements_file:
+        lines = requirements_file.read().splitlines()
+        for line in lines:
+            line = line.strip()
+            if not line or line.startswith('#') or line.startswith('"""'):
+                continue
+            elif line.startswith('-r'):
+                sub_requirements_path = line.split(' ')[-1]
+                results = results.union(
+                    parse(os.path.join(dir_path, sub_requirements_path)))
+            else:
+                results.add(re.split(r'[>=<]', line)[0])
+    return results
+
